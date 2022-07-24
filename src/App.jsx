6457931d7fd173a2d6b04f124e9dd6c9809afc42
mukhammadrizooff <legend.it@bk.ru>
2022-07-25 01:11:15 +0500

    Collect all components

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import Header from './components/header/Header'
+import Nav from './components/nav/Nav'
+import About from './components/about/About'
+import Experience from './components/experience/Experience'
+import Services from './components/services/Services'
+import Portfolio from './components/portfolio/Portfolio'
+import Recomendations from './components/recomendations/Recomendations'
+import Conatct from './components/contact/Conatct'
+import Footer from './components/footer/Footer'
+
+const App = () => {
+  return (
+    <>
+        <Header />
+        <Nav />
+        <About />
+        <Experience />
+        <Services />
+        <Portfolio />
+        <Recomendations />
+        <Conatct />
+        <Footer />
+    </>
+  )
+}
+
+export default App
