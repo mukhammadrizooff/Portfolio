381876797bd96e95164438e232b723b92034e275
mukhammadrizooff <legend.it@bk.ru>
2022-07-30 23:07:58 +0500

    Install swipper

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,10 +1,108 @@
-import React from 'react'
-import './portfolio.css'
+import React from 'react';
+import AnalyticsEventTracker from '../eventsTracker/AnalyticsEventTracker';
+import './portfolio.css';
+import ProjectImage1 from '../../assets/bookstore.png';
+import ProjectImage2 from '../../assets/leaderboard.png';
+import ProjectImage3 from '../../assets/mathma.png';
+import ProjectImage4 from '../../assets/paymodus.png';
+import ProjectImage5 from '../../assets/hollyflx.png';
+import ProjectImage6 from '../../assets/space.png';
 
-const Portfolio = () => {
+const projectData = [
+  {
+    id: 1,
+    image: ProjectImage1,
+    title: 'Bookstore CMS',
+    description: 'It is an app that allows you to display a list of added books, add new book and remove a book',
+    github: 'https://github.com/mukhammadrizooff/Bookstore',
+    demo: 'https://mukhammadrizooff.github.io/Bookstore/',
+  },
+
+  {
+    id: 2,
+    image: ProjectImage2,
+    title: 'Leaderboard',
+    description: 'An Api based application which adds and retrieves the scores from the leaderboard API',
+    github: 'https://github.com/mukhammadrizooff/Leaderboard',
+    demo: 'https://mukhammadrizooff.github.io/Leaderboard/dist/',
+  },
+
+  {
+    id: 3,
+    image: ProjectImage3,
+    title: 'Math Magicians',
+    description: 'This project is a simple calculator web application, using JavaScript and React. It consists of three pages, homepage, calculator and quote.',
+    github: 'https://github.com/mukhammadrizooff/Math-Magicians',
+    demo: 'https://mukhammadrizooff.github.io/Math-Magicians/',
+  },
+
+  {
+    id: 4,
+    image: ProjectImage4,
+    title: 'Pay Modus',
+    description: 'The Pay Modus App is going to be helpful to track your expenses and transactions.',
+    github: 'https://github.com/mukhammadrizooff/Budget-app',
+    demo: 'https://pay-modus-mukhammadrizooff.herokuapp.com/',
+  },
+
+  {
+    id: 5,
+    image: ProjectImage5,
+    title: 'Hollyflix',
+    github: 'https://github.com/mukhammadrizooff/Hollyflix-Capstone',
+    description: 'In this project, you can view the details of the movies and write your opinion in the comments and also set the time. In this project, we did three people together based on a kanban board plan.',
+    demo: 'https://sumairq.github.io/hollyflix-capstone/dist/',
+  },
+
+  {
+    id: 6,
+    image: ProjectImage6,
+    title: 'Space Traveler`s Hub',
+    github: 'https://github.com/mukhammadrizooff/Space-Travel-React',
+    description: 'Web application that allows users to book rockets, dragons, and join selected space missions using data from the SpaceX API.',
+    demo: 'https://sumairq.github.io/',
+  },
+];
+
+function Portfolio() {
+  const gaEventTracker = AnalyticsEventTracker('Visit Projects');
   return (
-    <section id='portfolio'>Portfolio</section>
-  )
+    <section id="portfolio">
+      <h5>My Recent work</h5>
+      <h2>Portfolio</h2>
+      <div className="container portfolio__container">
+        {projectData.map((project) => (
+          <article className="portfolio__item" key={project.id}>
+            <div className="portfolio__item-image">
+              <img src={project.image} alt="project thumbnail" />
+            </div>
+            <h3>{project.title}</h3>
+            <small>{project.description}</small>
+            <div className="cta">
+              <a
+                href={project.github}
+                className="btn"
+                target="_blank"
+                rel="noreferrer"
+                onClick={() => gaEventTracker('View Source Code')}
+              >
+                Source Code
+              </a>
+              <a
+                href={project.demo}
+                className="btn btn-primary"
+                target="_blank"
+                rel="noreferrer"
+                onClick={() => gaEventTracker('View Live Demo')}
+              >
+                Live Demo
+              </a>
+            </div>
+          </article>
+        ))}
+      </div>
+    </section>
+  );
 }
 
-export default Portfolio
+export default Portfolio;
--- src/components/recomendations/Recomendations.jsx
+++ src/components/recomendations/Recomendations.jsx
@@ -1,10 +1,61 @@
-import React from 'react'
-import './recomendations.css'
+import React from 'react';
+import { Pagination } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import client1 from '../../assets/sumair.jpeg';
+import client2 from '../../assets/david.jpeg';
+import client3 from '../../assets/gaby.jpeg';
+import './recomendations.css';
+import 'swiper/css';
+import 'swiper/css/pagination';
 
-const recomendations = () => {
-  return (
-    <section id='recomendations'>Recomendations</section>
-  )
-}
+const testimonials = [
+  {
+    id: 1,
+    avatar: client1,
+    name: 'Sumair Qaisar',
+    review:
+      'I have worked alongside Rizo while building multiple group projects. During our time together I have seen him, not only excel at the core programming skills but also other important soft skills like communication, problem-solving, brainstorming. His contributions have been critical to the successful completion of the projects. His incredible attention to detail and ambition set a great example for other team members as well. I look forward to working with Rizo who, besides his professional competencies, is also a very friendly and affable person. I have only optimistic predictions for his career trajectory.',
+  },
 
-export default recomendations
+  {
+    id: 2,
+    avatar: client2,
+    name: 'David Lafontant',
+    review:
+      'I had, and fortunately still have, the pleasure of being a teammate of Muhammad Rizo Abdunazarov on several projects. In addition to his proven skills in technologies such as JavaScript, React, and Ruby on Rails, he is someone who always helps the team focus. He leads by example. He is a problem solver and a hardworking person.',
+  },
+
+  {
+    id: 3,
+    avatar: client3,
+    name: 'Gabriela Sánchez Espirilla',
+    review:
+      'Muhammad is a talented programmer and proficient in working with technologies. We pair-programmed together as students at Microverse, where he proved his commitment to getting high-quality projects. I also realize that he has de ability to devise not just one but several ways to go about implementing algorithms and applications. This, along with his innate positive attitude, got us through the coding difficulties we faced, so I can assure you that he is an excellent coding partner and also a great friend who loves mentoring others. I am sure he will be successful in his efforts and will be an asset to whatever web development team is lucky to include him.',
+  },
+];
+
+const Recomendations = () => (
+  <section id="recomendations">
+    <h5>Recomendations From Other Developers</h5>
+    <h2>Recomendations</h2>
+    <Swiper
+      className="container testimonials__container"
+      modules={[Pagination]}
+      spaceBetween={40}
+      slidesPerView={1}
+      pagination={{ clickable: true }}
+    >
+      {testimonials.map((reviewer) => (
+        <SwiperSlide className="testimonial" key={reviewer.id}>
+          <div className="client__avatar">
+            <img src={reviewer.avatar} alt="client profile" />
+          </div>
+          <h5 className="client__name">{reviewer.name}</h5>
+          <small className="client__review">{reviewer.review}</small>
+        </SwiperSlide>
+      ))}
+    </Swiper>
+  </section>
+);
+
+export default Recomendations;
